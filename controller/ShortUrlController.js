fb1dd532c90980734bcbfc84be428ca4ffdfd7af
许晓辉 <xu_xiaohui7685@126.com>
2017-04-04 18:57:35 +0800

    页面状态修改

--- controller/ShortUrlController.js
+++ controller/ShortUrlController.js
@@ -16,18 +16,40 @@ module.exports = function (app) {
                 console.log('error');
                 throw err;
             }
-            res.render('index',{channelList:data});
-            console.log(JSON.stringify(data));
+            res.render('index2',{channelList:data});
+            console.log(data);
         });
     });
 
+    /** 删除渠道 **/
+    app.delete('/channel/:id', (req, res) => {
+        let id = req.params.id;
+        channelService.deleteItem(id).then(data => {
+            res.json({code:0,data:data});
+        },error => {});
+    });
+
+    /** 生成短链接 **/
     app.post('/makeUrl', urlencodedParser, (req, res) => {
         if (!req.body) return res.sendStatus(400);
         request.post({url:'http://dwz.cn/create.php', form: {url:req.body.url,alias:req.body.custom}},function(err,httpResponse,body){
             res.json(body);
         });
     });
+    /** 根据渠道名称搜索渠道列表 **/
+    app.post('/search',urlencodedParser, (req, res) => {
+        if (!req.body) return res.sendStatus(400);
+        let searchObj = req.body;
+        let searchParam = searchObj.searchText?{channelName:searchObj.searchText}:{};
+        channelService.fuzzyQuery(searchParam).then(list => {
+            res.json({code:0,channelList:list});
+        },error => {
+            console.log(error);
+        })
+        //console.log(requestArr);
+    });
 
+    /** 增加一个渠道信息 **/
     app.post('/submitChannel',urlencodedParser, (req, res) => {
         if (!req.body) return res.sendStatus(400);
         let requestArr = req.body;
--- services/channelService.js
+++ services/channelService.js
@@ -34,6 +34,30 @@ let channelService = {
                 resolve(list);
             })
         })
+    },
+    fuzzyQuery(params){
+        let query = {};
+        Object.keys(params).forEach(item => {
+            query[item] = new RegExp(params[item])
+        });
+        return new Promise((resolve, reject) => {
+            this.find(query).then(list => {
+                resolve(list);
+            },error => {
+                reject(error);
+            })
+        })
+    },
+    deleteItem(id) {
+        return new Promise((resolve,reject) => {
+            Channel.find({_id:id}).remove((err, data) => {
+                if (err) {
+                    reject(err);
+                    return;
+                }
+                resolve(data);
+            });
+        });
     }
 };
 
