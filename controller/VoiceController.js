7acdcf9f0da3cd8cac99fb2b199bbbad28a396f2
许晓辉 <xu_xiaohui7685@126.com>
2017-04-15 15:44:12 +0800

    no message

--- controller/VoiceController.js
+++ controller/VoiceController.js
@@ -1,6 +1,7 @@
 /**
  * Created by xuxiaohui on 2017/4/15.
  */
+let https = require('https');
 let bodyParser = require('body-parser');
 let jsonParser = bodyParser.json();
 let urlencodedParser = bodyParser.urlencoded({extended: false});
@@ -19,7 +20,7 @@ const CALLBACKURL = 'http://118.89.61.82:3000/voice/recognitionCallback';
 let capi = new Capi({
     serviceType: SERVICETYPE,
     path: PATH, //Api 请求路径
-    baseHost:BASEHOST, //Api 的基础域名. 与 `serviceType` 拼装成请求域名.
+    baseHost: BASEHOST, //Api 的基础域名. 与 `serviceType` 拼装成请求域名.
     SecretId: SECRETID,//secretId
     SecretKey: SECRETKEY//secretKey
 });
@@ -41,37 +42,65 @@ module.exports = function (app) {
             console.log(`audioTime:${result.audioTime}`);
             console.log('================eng=================');
         }
-        res.json({code:0,message:'成功'});
+        res.json({code: 0, message: '成功'});
     }),
-    app.get(`${PREFIX}/sendVoice`,(req,res) => {
-       /* let queryStr = capi.generateQueryString({
-            sub_service_type:0,
-            engine_model_type:1,
-            callback_url:CALLBACKURL,
-            res_text_format:0,
-            res_type:1,
-            source_type:0,
-            url:testUrl,
-            expired:1473752807
+        app.get(`${PREFIX}/sendVoice`, (req, res) => {
+            /* let queryStr = capi.generateQueryString({
+             sub_service_type:0,
+             engine_model_type:1,
+             callback_url:CALLBACKURL,
+             res_text_format:0,
+             res_type:1,
+             source_type:0,
+             url:testUrl,
+             expired:1473752807
+             });
+             let sigureText = capi.sign(`POST${SERVICETYPE}.${BASEHOST+PATH}?${queryStr}`,SECRETKEY);
+             let finalUrl = capi.generateUrl() + '?' + queryStr;*/
+            capi.request({
+                sub_service_type: 0,
+                engine_model_type: 1,
+                callback_url: CALLBACKURL,
+                res_text_format: 0,
+                res_type: 1,
+                source_type: 0,
+                url: testUrl,
+                expired: 1473752807
+            }, {
+                serviceType: 'aai',
+                method: 'POST'
+            }, (error, body) => {
+                console.log('abc')
+                console.log(JSON.stringify(body));
+            });
+            res.json('sendVoice');
         });
-        let sigureText = capi.sign(`POST${SERVICETYPE}.${BASEHOST+PATH}?${queryStr}`,SECRETKEY);
-        let finalUrl = capi.generateUrl() + '?' + queryStr;*/
-        capi.request({
-            sub_service_type:0,
-            engine_model_type:1,
-            callback_url:CALLBACKURL,
-            res_text_format:0,
-            res_type:1,
-            source_type:0,
-            url:testUrl,
-            expired:1473752807
-        },{
-            serviceType: 'aai',
-            method: 'POST'
-        },(error,body)=> {
-            console.log('abc')
-            console.log(JSON.stringify(body));
+
+    app.get(`${PREFIX}/sendVoices`, (req, res) => {
+        let queryStr = capi.generateQueryString({
+            sub_service_type: 0,
+            engine_model_type: 1,
+            callback_url: CALLBACKURL,
+            res_text_format: 0,
+            res_type: 1,
+            source_type: 0,
+            url: testUrl,
+            expired: 1473752807
         });
+        let sigureText = capi.sign(`POST${SERVICETYPE}.${BASEHOST + PATH}?${queryStr}`, SECRETKEY);
+        let finalUrl = capi.generateUrl() + '?' + queryStr;
+        let postOption = {
+            URL: finalUrl,
+            headers: {
+                'Host': `${SERVICETYPE}.${BASEHOST + PATH}`,
+                'Authorization': sigureText,
+                'Content-Type': 'application/octet-stream'
+            }
+        }
+        var post_req = https.request(postOption, function(error,body) {
+            console.log(`error:${JSON.stringify(error)}`);
+            console.log(`body:${JSON.stringify(body)}`)
+        })
         res.json('sendVoice');
     });
 };
