bb1f248712ef66803de5520208b0583c6e15e1f1
许晓辉 <xu_xiaohui7685@126.com>
2017-05-08 17:08:00 +0800

    为mongodb增加账号密码

--- dao/mongoose.js
+++ dao/mongoose.js
@@ -3,5 +3,5 @@
  */
 let mongoose = require('mongoose');
 //链接数据库
-mongoose.connect('mongodb://127.0.0.1:27017/channeldata');
+mongoose.connect('mongodb://xuxh:5885050@127.0.0.1:27017/channeldata');
 module.exports = mongoose;
